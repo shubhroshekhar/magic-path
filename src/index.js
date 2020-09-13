03c1b20acae78a1b7479df409df321126a71a659
Shubhro Shekhar <shubhro@symboinsurance.com>
2020-09-13 16:27:12 +0530

    eslint and eslint fixes and createObjectAsPerName added

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
-import merge from 'lodash/merge';
-import moment from 'moment';
+import merge from "lodash/merge";
+import moment from "moment";
+import { consoleLog } from "dim-logs";
 
 const formatDataAsKey = (key, val) => {
-  const [FT, FF] = key.split('$');
-  const [, TYPE] = FT.split('_');
-  if (TYPE === 'DATE') {
-    let format = { day: 'DD', month: 'MM', year: 'YYYY' };
+  const [FT, FF] = key.split("$");
+  const [, TYPE] = FT.split("_");
+  if (TYPE === "DATE") {
+    let format = { day: "DD", month: "MM", year: "YYYY" };
     const returnObj = {};
     try {
       format = JSON.parse(FF);
     } catch (e) {
-      console.log(e);
+      consoleLog(e);
     }
     const mmm = new moment(val);
     Object.keys(format).forEach(k => {
-      if (format[k] === 'DD') {
-        returnObj[k] = `${mmm.get('D')}`;
-      } else if (format[k] === 'MM') {
-        const month = `${mmm.get('M') + 1}`;
+      if (format[k] === "DD") {
+        returnObj[k] = `${mmm.get("D")}`;
+      } else if (format[k] === "MM") {
+        const month = `${mmm.get("M") + 1}`;
         returnObj[k] = month.length === 1 ? `0${month}` : month;
-      } else if (format[k] === 'YYYY') {
-        returnObj[k] = `${mmm.get('Y')}`;
+      } else if (format[k] === "YYYY") {
+        returnObj[k] = `${mmm.get("Y")}`;
       }
     });
     return returnObj;
-  } else if (TYPE === 'DATEASSTRING') {
-    let format = FF || 'DD/MM/YYYY';
-    let returnDate = '';
+  } else if (TYPE === "DATEASSTRING") {
+    let format = FF || "DD/MM/YYYY";
+    let returnDate = "";
     try {
       returnDate = moment(val).format(format);
     } catch (e) {
-      returnDate = moment(val).format('DD/MM/YYYY');
+      returnDate = moment(val).format("DD/MM/YYYY");
     }
     return returnDate;
   }
@@ -44,42 +45,61 @@ const getValueForKey = (o, kl) => {
   }
   if (kl.length > 1) {
     const [fst, ...oth] = kl;
-    if (fst === '[]') {
+    if (fst === "[]") {
       return o.map(obj => getValueForKey(obj, oth));
     } else if (new RegExp(/^\[\d+\]$/).test(fst)) {
-      const indexFst = parseInt(fst.replace('[', '').replace(']', ''), 10);
+      const indexFst = parseInt(fst.replace("[", "").replace("]", ""), 10);
       return getValueForKey(o[indexFst], oth);
-    } else if (fst.indexOf('FORMAT') >= 0) {
+    } else if (fst.indexOf("FORMAT") >= 0) {
       return getValueForKey(formatDataAsKey(fst, o), oth);
     }
     return getValueForKey(o[fst], oth);
   } else {
     const [fst] = kl;
     if (new RegExp(/^\[\d+\]$/).test(fst)) {
-      const indexFst = parseInt(fst.replace('[', '').replace(']', ''), 10);
+      const indexFst = parseInt(fst.replace("[", "").replace("]", ""), 10);
       return o[indexFst];
-    } else if (fst === '[]') {
+    } else if (fst === "[]") {
       return o;
-    } else if (fst.indexOf('FORMAT') >= 0) {
+    } else if (fst.indexOf("FORMAT") >= 0) {
       return formatDataAsKey(fst, o);
     }
     return fst ? o[fst] : o;
   }
 };
 
+const createObjectAsPerNameArray = (arr, val) => {
+  if (arr.length === 0) {
+    return val;
+  }
+  const [fst, ...oth] = arr;
+  if (fst === "[]") {
+    return [createObjectAsPerNameArray(oth, val)];
+  }
+  return { [fst]: createObjectAsPerNameArray(oth, val) };
+};
+
+const createObjectAsPerName = (oName, value) => {
+  if (!oName) {
+    return value;
+  }
+  const nameArray = oName.split(".");
+  return createObjectAsPerNameArray(nameArray, value);
+};
+
 
 const mapResponse = (data, mappining = {}) => {
   if (Array.isArray(mappining) && mappining.length) {
     return data.map(d => mapResponse(d, mappining[0]));
-  } else if (typeof mappining === 'object' && Object.keys(mappining).length) {
+  } else if (typeof mappining === "object" && Object.keys(mappining).length) {
     let responseData = {};
     Object.keys(mappining).forEach(key => {
       const d = createObjectAsPerName(key, mapResponse(data, mappining[key]));
       responseData = merge(responseData, d);
     });
     return responseData;
-  } else if (typeof mappining === 'string' && mappining.length) {
-    return getValueForKey(data, mappining.split('.'));
+  } else if (typeof mappining === "string" && mappining.length) {
+    return getValueForKey(data, mappining.split("."));
   }
   return data;
 };
@@ -87,7 +107,8 @@ const mapResponse = (data, mappining = {}) => {
 const get = (data, mapping, defaultValue) => {
   const value = mapResponse(data, mapping);
   return value || defaultValue;
-}
+};
+
 
 
 export {
